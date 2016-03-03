17213432d4ad380b61bf247f0eb6c67f1cfe1d5a
Bharani Thungapalli <bthungapalli@NIST-MAC-L-0013s-MacBook-Pro.local>
2016-03-03 20:43:01 +0530

    Date Picker

---
+++ public/angularJs/directives/datePickerDirective.js
@@ -0,0 +1,21 @@
+angular.module("challengeMeApp").directive('datepicker', function() {
+  return {
+    restrict: "A",
+    require: "ngModel",
+    link: function (scope, elem, attrs, ngModelCtrl) {
+      var updateModel = function (dateText) {
+        scope.$apply(function () {
+          ngModelCtrl.$setViewValue(dateText);
+        });
+      };
+      var options = {
+        dateFormat: "dd/mm/yy",
+        minDate:"0d",
+        onSelect: function (dateText) {
+          updateModel(dateText);
+        }
+      };
+      elem.datepicker(options);
+    }
+  }
+});
