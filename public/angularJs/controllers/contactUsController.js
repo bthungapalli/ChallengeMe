5b7e2c8e84142a0e507c871de295e45351a3aa65
unknown <apatha@APATHA.nisum.com>
2016-03-28 22:36:08 +0530

    contact us changes

--- public/angularJs/controllers/contactUsController.js
+++ public/angularJs/controllers/contactUsController.js
@@ -14,6 +14,10 @@ $scope.errorMessage="";
 		$http.post(challengeMeConstants.contactUs,$scope.contactUs).success(function(response){
 			$scope.redirectToLoginIfSessionExpires(response);
 			$scope.successMessage="Thank you for contacting us."
+				$scope.contactUs={
+					"subject":"",
+					"query":""
+			};
 			$loading.finish('contactUs');
 			}).error(function(error){
 				$scope.errorMessage=challengeMeConstants.errorMessage;
